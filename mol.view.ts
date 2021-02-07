5992cb20cec1292cb2f1a69367c2238c6404c965
jin <jin@hyoo.ru>
2021-02-07 18:11:29 +0800

    More sections

--- mol.view.ts
+++ mol.view.ts
@@ -3,18 +3,25 @@ namespace $.$$ {
 		
 		pages() {
 
-			const app = this.$.$mol_state_arg.value( 'app' )
+			const section = this.$.$mol_state_arg.value( 'section' )
 			
 			return [
 				this.Menu(),
-				... ( app === 'components' ) ?  this.Components_app().pages() : [],
-				... ( app === 'view.tree' ) ?  this.View_tree_app().pages() : [],
-				... ( app === 'articles' ) ?  this.Articles_app().pages() : [],
-				//... ( app === 'icons' ) ?  [ this.Icons_app() ] : [],
-				// ... ( app === 'slides' ) ?  [ this.Slides_app() ] : [],
+				... ( section === 'main' ) ?  [ this.Main() ] : [],
+				... ( section === 'components' ) ?  this.Components_app().pages() : [],
+				... ( section === 'view.tree' ) ?  this.View_tree_app().pages() : [],
+				... ( section === 'articles' ) ?  this.Articles_app().pages() : [],
+				... ( section === 'apps' ) ?  this.Apps_app().pages() : [],
+				... ( section === 'icons' ) ?  [ this.Icons_app() ] : [],
+				... ( section === 'slides' ) ?  [ this.Slides_app() ] : [],
 			]
 
 		}
 
+		@ $mol_mem
+		description() {
+			return $mol_file.relative( 'mol/readme.md' ).text() as string
+		}
+		
 	}
 }
