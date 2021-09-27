20b198a7ea8c01e598386455e32b1e5f60cbb0cd
jin <jin@hyoo.ru>
2021-09-27 19:10:43 +0300

    Port to $mol_book2_catalog

--- mol.view.ts
+++ mol.view.ts
@@ -1,23 +1,6 @@
 namespace $.$$ {
 	export class $hyoo_mol extends $.$hyoo_mol {
 		
-		pages() {
-
-			const section = this.$.$mol_state_arg.value( 'section' )
-			
-			return [
-				this.Menu(),
-				... ( section === 'main' ) ?  [ this.Main() ] : [],
-				... ( section === 'components' ) ?  this.Components_app().pages() : [],
-				... ( section === 'view.tree' ) ?  this.View_tree_app().pages() : [],
-				... ( section === 'articles' ) ?  this.Articles_app().pages() : [],
-				... ( section === 'apps' ) ?  this.Apps_app().pages() : [],
-				... ( section === 'icons' ) ?  [ this.Icons_app() ] : [],
-				... ( section === 'slides' ) ?  [ this.Slides_app() ] : [],
-			]
-
-		}
-
 		@ $mol_mem
 		description() {
 			return $mol_file.relative( 'mol/readme.md' ).text() as string
