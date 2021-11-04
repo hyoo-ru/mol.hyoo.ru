53e99407b1aab95733c114c8f2d39689e78ab3fb
nin-jin <nin-jin@users.noreply.github.com>
2021-11-04 05:32:55 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -2041,6 +2041,7 @@ declare namespace $ {
         type(val?: any): string;
         link(val?: any): string;
         title(val?: any): string;
+        Image(): $mol_image;
     }
 }
 
@@ -3209,6 +3210,7 @@ declare namespace $ {
         Menu_item(id: any): $$.$mol_link;
         menu_options(): {
             "https://nin-jin.github.io/slides/slides/": string;
+            "https://nin-jin.github.io/slides/reactivity/": string;
             "https://nin-jin.github.io/slides/orp/": string;
             "https://nin-jin.github.io/slides/tree/": string;
             "https://nin-jin.github.io/slides/sourcemap/": string;
--- web.d.ts
+++ web.d.ts
@@ -2058,6 +2058,7 @@ declare namespace $ {
         type(val?: any): string;
         link(val?: any): string;
         title(val?: any): string;
+        Image(): $mol_image;
     }
 }
 
@@ -3195,6 +3196,7 @@ declare namespace $ {
         Menu_item(id: any): $$.$mol_link;
         menu_options(): {
             "https://nin-jin.github.io/slides/slides/": string;
+            "https://nin-jin.github.io/slides/reactivity/": string;
             "https://nin-jin.github.io/slides/orp/": string;
             "https://nin-jin.github.io/slides/tree/": string;
             "https://nin-jin.github.io/slides/sourcemap/": string;
