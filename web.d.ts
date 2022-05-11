f5c8f6717b8fb236db68e14570d91f9ed935a086
nin-jin <nin-jin@users.noreply.github.com>
2022-05-11 11:36:28 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.d.ts
+++ web.d.ts
@@ -1723,6 +1723,8 @@ declare namespace $ {
         code3: RegExp;
         code: RegExp;
         strike: RegExp;
+        remark: RegExp;
+        quote: RegExp;
         'image-link': RegExp;
         'text-link': RegExp;
         'text-link-http': RegExp;
