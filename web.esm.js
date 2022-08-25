2c723ce0273fb1065619a1372fa0936eb6a700e1
nin-jin <nin-jin@users.noreply.github.com>
2022-08-25 10:08:13 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.esm.js
+++ web.esm.js
@@ -5274,6 +5274,9 @@ var $;
         generate(params) {
             return null;
         }
+        get native() {
+            return new RegExp(this.source, this.flags);
+        }
         static repeat(source, min = 0, max = Number.POSITIVE_INFINITY) {
             const regexp = $mol_regexp.from(source);
             const upper = Number.isFinite(max) ? max : '';
@@ -13662,7 +13665,7 @@ var $;
                 ]), 1),
             ],
         },
-    });
+    }).native;
 })($ || ($ = {}));
 //hyoo/crowd/tokenizer/tokenizer.ts
 ;
@@ -14786,7 +14789,7 @@ var $;
                 --from;
                 next = String(list[from].data) + next;
             }
-            const words = [...next.matchAll($hyoo_crowd_tokenizer)].map(token => token[0]);
+            const words = next.match($hyoo_crowd_tokenizer);
             this.as($hyoo_crowd_list).insert(words, from, to);
             return this;
         }
