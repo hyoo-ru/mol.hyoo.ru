b367e1e34db209425ec18bb1709fc6561300717b
nin-jin <nin-jin@users.noreply.github.com>
2022-09-27 07:34:19 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.esm.js
+++ node.esm.js
@@ -14998,13 +14998,10 @@ var $;
             land_inner.apply(land_outer.delta());
             return land_inner;
         }
-        async delta_land(land, clocks = [new $hyoo_crowd_clock, new $hyoo_crowd_clock]) {
-            const units = land.delta(clocks);
-            if (!units.length)
-                return [];
-            for (const unit of units) {
+        sign_units(units) {
+            return Promise.all(units.map(async (unit) => {
                 if (unit.bin)
-                    continue;
+                    return unit;
                 const bin = $hyoo_crowd_unit_bin.from_unit(unit);
                 let sign = this._signs.get(unit);
                 if (!sign) {
@@ -15014,8 +15011,11 @@ var $;
                 bin.sign(sign);
                 unit.bin = bin;
                 this._signs.set(unit, sign);
-            }
-            return units;
+                return unit;
+            }));
+        }
+        delta_land(land, clocks = [new $hyoo_crowd_clock, new $hyoo_crowd_clock]) {
+            return this.sign_units(land.delta(clocks));
         }
         async delta_batch(land, clocks = [new $hyoo_crowd_clock, new $hyoo_crowd_clock]) {
             const units = await this.delta_land(land, clocks);
@@ -15184,6 +15184,9 @@ var $;
             return land;
         }
     }
+    __decorate([
+        $mol_action
+    ], $hyoo_crowd_reg.prototype, "yoke", null);
     $.$hyoo_crowd_reg = $hyoo_crowd_reg;
 })($ || ($ = {}));
 //hyoo/crowd/reg/reg.ts
--- web.js
+++ web.js
@@ -14512,13 +14512,10 @@ var $;
             land_inner.apply(land_outer.delta());
             return land_inner;
         }
-        async delta_land(land, clocks = [new $hyoo_crowd_clock, new $hyoo_crowd_clock]) {
-            const units = land.delta(clocks);
-            if (!units.length)
-                return [];
-            for (const unit of units) {
+        sign_units(units) {
+            return Promise.all(units.map(async (unit) => {
                 if (unit.bin)
-                    continue;
+                    return unit;
                 const bin = $hyoo_crowd_unit_bin.from_unit(unit);
                 let sign = this._signs.get(unit);
                 if (!sign) {
@@ -14528,8 +14525,11 @@ var $;
                 bin.sign(sign);
                 unit.bin = bin;
                 this._signs.set(unit, sign);
-            }
-            return units;
+                return unit;
+            }));
+        }
+        delta_land(land, clocks = [new $hyoo_crowd_clock, new $hyoo_crowd_clock]) {
+            return this.sign_units(land.delta(clocks));
         }
         async delta_batch(land, clocks = [new $hyoo_crowd_clock, new $hyoo_crowd_clock]) {
             const units = await this.delta_land(land, clocks);
@@ -14698,6 +14698,9 @@ var $;
             return land;
         }
     }
+    __decorate([
+        $mol_action
+    ], $hyoo_crowd_reg.prototype, "yoke", null);
     $.$hyoo_crowd_reg = $hyoo_crowd_reg;
 })($ || ($ = {}));
 //hyoo/crowd/reg/reg.ts
