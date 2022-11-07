261ce33d4949e1f353a3d3ef0756390319a44f0b
nin-jin <nin-jin@users.noreply.github.com>
2022-11-07 07:10:03 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.js
+++ node.js
@@ -15406,6 +15406,7 @@ var $;
             const join_unit = new $hyoo_crowd_unit(this.id(), auth.id, auth.id, auth.id, '0_0', '0_0', time, auth.key_public_serial, null);
             this._unit_all.set(auth_id, join_unit);
             this._joined = true;
+            this.pub.emit();
         }
         leave() {
             const auth = this.peer();
@@ -15421,6 +15422,7 @@ var $;
             const join_unit = new $hyoo_crowd_unit(this.id(), auth.id, auth.id, auth.id, '0_0', '0_0', time, null, null);
             this._unit_all.set(auth_id, join_unit);
             this._joined = false;
+            this.pub.emit();
         }
         level_base(next) {
             this.level('0_0', next);
@@ -15482,10 +15484,12 @@ var $;
             return authors;
         }
         first_stamp() {
+            this.pub.promote();
             const grab_unit = this._unit_all.get(`${this.id()}/${this.id()}`);
             return (grab_unit && $hyoo_crowd_time_stamp(grab_unit.time)) ?? null;
         }
         last_stamp() {
+            this.pub.promote();
             return this.clock_data.last_stamp();
         }
         selection(peer) {
