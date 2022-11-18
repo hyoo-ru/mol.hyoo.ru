bf0524ac14f17b0a631663dd45ebe4d0a5ae83cf
nin-jin <nin-jin@users.noreply.github.com>
2022-11-18 04:02:25 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.d.ts
+++ web.d.ts
@@ -2423,11 +2423,13 @@ declare namespace $ {
             src: string;
             alt: string;
             loading: string;
+            decoding: string;
         };
         minimal_width(): number;
         minimal_height(): number;
         uri(): string;
         loading(): string;
+        decoding(): string;
     }
 }
 
--- web.esm.js
+++ web.esm.js
@@ -7790,7 +7790,8 @@ var $;
                 ...super.field(),
                 src: this.uri(),
                 alt: this.title(),
-                loading: this.loading()
+                loading: this.loading(),
+                decoding: this.decoding()
             };
         }
         minimal_width() {
@@ -7804,6 +7805,9 @@ var $;
         }
         loading() {
             return "eager";
+        }
+        decoding() {
+            return "async";
         }
     }
     $.$mol_image = $mol_image;
