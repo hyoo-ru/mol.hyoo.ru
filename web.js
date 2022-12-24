f98b47ae4e3dafb27684d2a3c1d9b2c99ffb1439
nin-jin <nin-jin@users.noreply.github.com>
2022-12-24 19:00:41 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.js
+++ web.js
@@ -8590,6 +8590,8 @@ var $;
             obj.numb_showed = () => false;
             obj.highlight = () => this.highlight();
             obj.text = () => this.line_text(id);
+            obj.uri_resolve = (id) => this.uri_resolve(id);
+            obj.syntax = () => this.code_syntax();
             return obj;
         }
         Link(id) {
@@ -8665,6 +8667,9 @@ var $;
         }
         line_content(id) {
             return [];
+        }
+        code_syntax() {
+            return null;
         }
         link_uri(id) {
             return "";
@@ -8847,6 +8852,9 @@ var $;
                     return null;
                 const url = new URL(uri, this.uri_base());
                 return url.toString();
+            }
+            code_syntax() {
+                return this.$.$mol_syntax2_md_code;
             }
             block_text(index) {
                 const token = this.flow_tokens()[index];
