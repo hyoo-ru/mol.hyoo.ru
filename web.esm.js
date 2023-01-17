15b0a0396ff7d331795cbf717385e677738c7865
nin-jin <nin-jin@users.noreply.github.com>
2023-01-17 07:06:22 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.esm.js
+++ web.esm.js
@@ -37079,7 +37079,7 @@ var $;
 //hyoo/bench/app/app.view.ts
 ;
 "use strict";
-let $hyoo_sync_revision = "8596b61";
+let $hyoo_sync_revision = "6f9327e";
 //hyoo/sync/-meta.tree/revision.meta.tree.ts
 ;
 "use strict";
@@ -37159,7 +37159,7 @@ var $;
         land_init(land) {
             this.db_land_init(land);
             if (!land.grabbed())
-                this.$.$mol_wait_timeout(10_000);
+                this.$.$mol_wait_timeout(1000);
         }
         land(id) {
             return this.world().land_sync(id);
