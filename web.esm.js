030b2e136a16ba24d1f69c6c730860f13a4b7bdc
nin-jin <nin-jin@users.noreply.github.com>
2023-01-31 09:53:40 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.esm.js
+++ web.esm.js
@@ -27906,18 +27906,19 @@ var $;
             const min = this.min - this.padding();
             const diff = limit - min;
             let pos = this.pos + this.before();
-            shrink: if (diff < 0) {
-                let mult = diff / min;
-                if (!Number.isFinite(mult))
-                    mult = 0;
-                for (const kid of this.kids) {
-                    kid.pos = pos;
-                    pos += kid.size = Math.min(limit, kid.min + Math.floor(kid.shrink() * mult));
-                    kid.down();
+            if (diff < 0)
+                shrink: {
+                    let mult = diff / min;
+                    if (!Number.isFinite(mult))
+                        mult = 0;
+                    for (const kid of this.kids) {
+                        kid.pos = pos;
+                        pos += kid.size = Math.min(limit, kid.min + Math.floor(kid.shrink() * mult));
+                        kid.down();
+                    }
                 }
-            }
-            else
-                grow: if (diff > 0) {
+            else if (diff > 0)
+                grow: {
                     let mult = diff / this.grow();
                     if (!Number.isFinite(mult))
                         mult = 0;
@@ -27927,14 +27928,14 @@ var $;
                         kid.down();
                     }
                 }
-                else
-                    fit: {
-                        for (const kid of this.kids) {
-                            kid.pos = pos;
-                            pos += kid.size = kid.min;
-                            kid.down();
-                        }
+            else
+                fit: {
+                    for (const kid of this.kids) {
+                        kid.pos = pos;
+                        pos += kid.size = kid.min;
+                        kid.down();
                     }
+                }
         }
     }
     $.$mol_layout_flex = $mol_layout_flex;
