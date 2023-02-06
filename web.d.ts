e12f11e2b38201b76bb838f8cfcd8bdd78619685
nin-jin <nin-jin@users.noreply.github.com>
2023-02-06 16:22:20 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.d.ts
+++ web.d.ts
@@ -1659,6 +1659,7 @@ declare namespace $ {
         line_height(): number;
         letter_width(): number;
         width_limit(): number;
+        row_width(): number;
         sub(): readonly any[];
     }
 }
@@ -2673,6 +2674,7 @@ declare namespace $ {
         Paragraph(id: any): $$.$mol_paragraph;
         Quote(id: any): $$.$mol_text;
         List(id: any): $mol_text_list;
+        item_index(id: any): number;
         Header(id: any): $$.$mol_text_header;
         Pre(id: any): $$.$mol_text_code;
         Cut(id: any): $mol_view;
@@ -2748,6 +2750,7 @@ declare namespace $.$$ {
             [x: string]: string;
         };
         list_type(index: number): string;
+        item_index(index: number): number;
         pre_text(index: number): string;
         quote_text(index: number): string;
         list_text(index: number): string;
@@ -7205,7 +7208,7 @@ declare namespace $.$$ {
         dom(): HTMLElement;
         sub(): $mol_view[];
         content(node: Node): $mol_view[];
-        views(node: Node): $mol_view[] | $mol_paragraph[] | $mol_html_view_heading[] | $mol_link_iconed[];
+        views(node: Node): $mol_view[] | $mol_paragraph[] | $mol_link_iconed[];
         text(node: Node): string;
         safe_link(uri: string): string;
         link_uri(node: HTMLAnchorElement): string;
@@ -11428,8 +11431,18 @@ declare namespace $ {
         attr(): {
             mol_text_list_type: string;
         };
+        Paragraph(id: any): $mol_text_list_item;
         type(): string;
     }
+    class $mol_text_list_item extends $mol_paragraph {
+        attr(): {
+            mol_text_list_item_index: number;
+        };
+        index(): number;
+    }
+}
+
+declare namespace $ {
 }
 
 declare namespace $ {
