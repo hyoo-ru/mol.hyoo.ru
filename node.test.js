e64ca391bc8ecabf4948d37b43790baa146406b9
nin-jin <nin-jin@users.noreply.github.com>
2023-04-25 07:06:39 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -21309,7 +21309,6 @@ var $;
                 const id = this.side_current_id();
                 const book = this.book_id();
                 return [
-                    this.News(),
                     this.Gap('left'),
                     ...book ? [this.Side_menu()] : [],
                     this.View(id),
