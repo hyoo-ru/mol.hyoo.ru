54f6be8a7447f20c6a64d98c9c439fb0315fe6bf
Alexey Alexeyev <ne4to777@gmail.com>
2023-06-09 01:47:29 +0300

    mol to habhub redirection

--- mol.view.ts
+++ mol.view.ts
@@ -1,5 +1,16 @@
 namespace $.$$ {
 	export class $hyoo_mol extends $.$hyoo_mol {
+
+		@$mol_mem
+		hubhabRedirect() {
+			if (this.$.$mol_state_arg.value('section') === 'articles') {
+				document.location.assign('https://habhub.hyoo.ru/' + document.location.hash)
+			}
+		}
+
+		auto() {
+			this.hubhabRedirect()
+		}
 		
 		Placeholder() {
 			if( this.spread() === 'docs' ) return this.Docs().Placeholder()
