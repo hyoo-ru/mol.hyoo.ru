4b6c95fb16fc3b631f20549710b911d0d882889d
nin-jin <nin-jin@users.noreply.github.com>
2023-08-29 18:13:32 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.js
+++ node.js
@@ -14157,6 +14157,7 @@ var $;
             }
             click(event) {
                 const cb = $mol_wire_sync(this.$.$mol_dom_context.navigator.clipboard);
+                cb.writeText(this.text());
                 cb.write(this.attachments());
             }
         }
