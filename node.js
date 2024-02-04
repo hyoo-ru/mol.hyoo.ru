df190534a10544584a6c3c82790764dc5c7753a5
nin-jin <nin-jin@users.noreply.github.com>
2024-02-04 11:30:13 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.js
+++ node.js
@@ -863,7 +863,7 @@ var $;
                 }
             }
             catch (e) {
-                console.log(e.stack);
+                e.message += '\n' + e.stack;
                 throw e;
             }
         }
