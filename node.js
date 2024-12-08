8ae3992de5d012574cfba889304fbaad24479bee
nin-jin <nin-jin@users.noreply.github.com>
2024-12-08 07:09:19 +0000

    Deploying to gh-pages from @ hyoo-ru/mol.hyoo.ru@83fa3fd6c3ddeff5a627ddd4f3ba9eede953ec0f 🚀

--- node.js
+++ node.js
@@ -13330,7 +13330,7 @@ var $;
             display: 'block',
             position: 'relative',
             font: {
-                family: 'monospace',
+                family: 'inherit',
             },
             Numb: {
                 textAlign: 'right',
