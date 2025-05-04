6606956e0377be453dd28f4140021226ea04a71b
nin-jin <nin-jin@users.noreply.github.com>
2025-05-04 08:37:15 +0000

    Deploying to gh-pages from @ hyoo-ru/mol.hyoo.ru@83fa3fd6c3ddeff5a627ddd4f3ba9eede953ec0f 🚀

--- node.js
+++ node.js
@@ -23572,7 +23572,7 @@ var $;
 			return obj;
 		}
 		type(){
-			return "tel";
+			return "text";
 		}
 		value_string(next){
 			if(next !== undefined) return next;
@@ -23591,6 +23591,7 @@ var $;
 		String(){
 			const obj = new this.$.$mol_string();
 			(obj.type) = () => ((this.type()));
+			(obj.keyboard) = () => ("decimal");
 			(obj.value) = (next) => ((this.value_string(next)));
 			(obj.hint) = () => ((this.hint()));
 			(obj.enabled) = () => ((this.string_enabled()));
--- node.test.js
+++ node.test.js
@@ -23563,7 +23563,7 @@ var $;
 			return obj;
 		}
 		type(){
-			return "tel";
+			return "text";
 		}
 		value_string(next){
 			if(next !== undefined) return next;
@@ -23582,6 +23582,7 @@ var $;
 		String(){
 			const obj = new this.$.$mol_string();
 			(obj.type) = () => ((this.type()));
+			(obj.keyboard) = () => ("decimal");
 			(obj.value) = (next) => ((this.value_string(next)));
 			(obj.hint) = () => ((this.hint()));
 			(obj.enabled) = () => ((this.string_enabled()));
