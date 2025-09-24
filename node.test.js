8b1954540b562ae9701ff2eb901623a260a23983
nin-jin <nin-jin@users.noreply.github.com>
2025-09-24 21:31:24 +0000

    Deploying to gh-pages from @ hyoo-ru/mol.hyoo.ru@3c646675003b58dfdbd9ce03a04a1ebb5e0cf0eb 🚀

--- node.test.js
+++ node.test.js
@@ -43306,9 +43306,11 @@ var $;
             reset(next) {
                 this.state(null);
             }
-            result(next = '') {
+            result(next) {
                 this.state();
-                return next;
+                if (next instanceof Error)
+                    next = next.message || this.message_invalid();
+                return next ?? '';
             }
             buttons() {
                 return [
@@ -43318,10 +43320,6 @@ var $;
                 ];
             }
             submit(next) {
-                if (!this.submit_allowed()) {
-                    this.result(this.message_invalid());
-                    return;
-                }
                 const tasks = Object.entries(this.state()).map(([field, next]) => () => {
                     const prev = this.model_pick(field);
                     return {
@@ -43329,10 +43327,23 @@ var $;
                         next: normalize_val(prev, next)
                     };
                 });
-                const normalized = $mol_wire_race(...tasks);
-                $mol_wire_race(...normalized.map(({ field, next }) => () => this.model_pick(field, next)));
+                try {
+                    if (!this.submit_allowed()) {
+                        throw new Error(this.message_invalid());
+                    }
+                    const normalized = $mol_wire_race(...tasks);
+                    $mol_wire_race(...normalized.map(({ field, next }) => () => this.model_pick(field, next)));
+                }
+                catch (e) {
+                    if ($mol_promise_like(e))
+                        $mol_fail_hidden(e);
+                    $mol_fail_log(e);
+                    this.result(e);
+                    return false;
+                }
                 this.reset();
                 this.result(this.message_done());
+                return true;
             }
         }
         __decorate([
