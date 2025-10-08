724060607a5f42cb59c1056f7c29a27456d388e0
nin-jin <nin-jin@users.noreply.github.com>
2025-10-08 17:14:40 +0000

    Deploying to gh-pages from @ hyoo-ru/mol.hyoo.ru@3c646675003b58dfdbd9ce03a04a1ebb5e0cf0eb 🚀

--- node.js
+++ node.js
@@ -4447,7 +4447,6 @@ var $;
         }
         dom_node_actual() {
             const node = this.dom_node();
-            $mol_dom_render_styles(node, this.style_size());
             const attr = this.attr();
             const style = this.style();
             $mol_dom_render_attributes(node, attr);
@@ -4546,12 +4545,6 @@ var $;
         attr() {
             return {
                 mol_theme: this.theme() ?? undefined,
-            };
-        }
-        style_size() {
-            return {
-                minHeight: this.minimal_height(),
-                minWidth: this.minimal_width(),
             };
         }
         style() {
--- node.test.js
+++ node.test.js
@@ -4438,7 +4438,6 @@ var $;
         }
         dom_node_actual() {
             const node = this.dom_node();
-            $mol_dom_render_styles(node, this.style_size());
             const attr = this.attr();
             const style = this.style();
             $mol_dom_render_attributes(node, attr);
@@ -4537,12 +4536,6 @@ var $;
         attr() {
             return {
                 mol_theme: this.theme() ?? undefined,
-            };
-        }
-        style_size() {
-            return {
-                minHeight: this.minimal_height(),
-                minWidth: this.minimal_width(),
             };
         }
         style() {
