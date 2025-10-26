ef71acbbd013f3a0b5c7b1934a8c53a044140fda
nin-jin <nin-jin@users.noreply.github.com>
2025-10-26 11:53:13 +0000

    Deploying to gh-pages from @ hyoo-ru/mol.hyoo.ru@3c646675003b58dfdbd9ce03a04a1ebb5e0cf0eb 🚀

--- node.js
+++ node.js
@@ -26827,8 +26827,6 @@ var $;
         }
         function clone(el) {
             const re = el.cloneNode();
-            if (el === root)
-                document.body.appendChild(re);
             if (el instanceof HTMLImageElement && !/^(data|blob):/.test(el.src)) {
                 const canvas = $mol_jsx("canvas", { width: el.naturalWidth, height: el.naturalHeight });
                 const context = canvas.getContext('2d');
@@ -26869,8 +26867,6 @@ var $;
                 restyle(kid, after);
                 re.appendChild(kid);
             }
-            if (el === root)
-                document.body.removeChild(re);
             return re;
         }
         const { width, height } = root.getBoundingClientRect();
