3236704942828e87f75bcbfaa7e2fbe9580d8430
nin-jin <nin-jin@users.noreply.github.com>
2026-01-24 14:23:53 +0000

    Deploying to gh-pages from @ hyoo-ru/mol.hyoo.ru@3c646675003b58dfdbd9ce03a04a1ebb5e0cf0eb 🚀

--- node.d.ts
+++ node.d.ts
@@ -3914,7 +3914,7 @@ declare namespace $ {
 }
 
 declare namespace $ {
-    type $mol_time_duration_config = number | string | {
+    type $mol_time_duration_config = number | string | [number, number, number, number, number, number] | {
         year?: number;
         month?: number;
         day?: number;
@@ -3937,6 +3937,7 @@ declare namespace $ {
         valueOf(): number;
         toJSON(): string;
         toString(pattern?: string): string;
+        toArray(): readonly [number, number, number, number, number, number];
         [Symbol.toPrimitive](mode: 'default' | 'number' | 'string'): string | number;
         static patterns: {
             '#Y': (duration: $mol_time_duration) => string;
@@ -3969,7 +3970,7 @@ declare namespace $ {
         saturday = 5,
         sunday = 6
     }
-    type $mol_time_moment_config = number | Date | string | {
+    type $mol_time_moment_config = number | Date | string | readonly (number | undefined)[] | {
         year?: number;
         month?: number;
         day?: number;
@@ -3999,6 +4000,7 @@ declare namespace $ {
         valueOf(): number;
         toJSON(): string;
         toString(pattern?: string): string;
+        toArray(): readonly [number | undefined, number | undefined, number | undefined, number | undefined, number | undefined, number | undefined, number | undefined];
         [Symbol.toPrimitive](mode: 'default' | 'number' | 'string'): string | number;
         [$mol_dev_format_head](): any[];
         static patterns: {
