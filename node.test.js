bad35df63a85a96599a149b9190d5b8eaf3dbe3e
nin-jin <nin-jin@users.noreply.github.com>
2026-05-15 07:05:38 +0000

    Deploying to gh-pages from @ hyoo-ru/mol.hyoo.ru@3c646675003b58dfdbd9ce03a04a1ebb5e0cf0eb 🚀

--- node.test.js
+++ node.test.js
@@ -785,6 +785,9 @@ var $;
         // [ Symbol.toPrimitive ]() {
         // 	return this.toString()
         // }
+        match(...args) {
+            return this.toString().match(...args);
+        }
         split(...args) {
             return this.toString().split(...args);
         }
