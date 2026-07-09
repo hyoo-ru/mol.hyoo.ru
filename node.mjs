c765d6c8e28dc7f2df1111e2d529c0ad10215559
nin-jin <nin-jin@users.noreply.github.com>
2026-07-09 10:18:24 +0000

    Deploying to gh-pages from @ hyoo-ru/mol.hyoo.ru@3c646675003b58dfdbd9ce03a04a1ebb5e0cf0eb 🚀

--- node.mjs
+++ node.mjs
@@ -19368,6 +19368,9 @@ var $;
 
 ;
 	($.$mol_form_field) = class $mol_form_field extends ($.$mol_labeler) {
+		state(){
+			return null;
+		}
 		name(){
 			return "";
 		}
@@ -19381,6 +19384,9 @@ var $;
 		}
 		control(){
 			return null;
+		}
+		attr(){
+			return {...(super.attr()), "mol_form_field_state": (this.state())};
 		}
 		bids(){
 			return [];
@@ -19409,6 +19415,9 @@ var $;
          * @see https://mol.hyoo.ru/#!section=demos/demo=mol_form_demo
          */
         class $mol_form_field extends $.$mol_form_field {
+            state() {
+                return this.bid() ? 'bid' : null;
+            }
             bid() {
                 return this.bids().filter(Boolean)[0] ?? '';
             }
--- node.test.js
+++ node.test.js
@@ -19359,6 +19359,9 @@ var $;
 
 ;
 	($.$mol_form_field) = class $mol_form_field extends ($.$mol_labeler) {
+		state(){
+			return null;
+		}
 		name(){
 			return "";
 		}
@@ -19372,6 +19375,9 @@ var $;
 		}
 		control(){
 			return null;
+		}
+		attr(){
+			return {...(super.attr()), "mol_form_field_state": (this.state())};
 		}
 		bids(){
 			return [];
@@ -19400,6 +19406,9 @@ var $;
          * @see https://mol.hyoo.ru/#!section=demos/demo=mol_form_demo
          */
         class $mol_form_field extends $.$mol_form_field {
+            state() {
+                return this.bid() ? 'bid' : null;
+            }
             bid() {
                 return this.bids().filter(Boolean)[0] ?? '';
             }
